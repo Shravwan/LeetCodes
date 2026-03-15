34af10dfc486415712334a6017fb375792740592
R.Shravwan <shravfifa@gmail.com>
2026-03-15 15:58:49 +0530

    Time: 2 ms (92.45%), Space: 13.8 MB (13.93%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        j=-1
+        for i in range(n):
+            if nums[i]==0:
+                j=i
+                break
+        if j==-1:
+            return nums
+        for i in range(j+1,n):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+        return nums
